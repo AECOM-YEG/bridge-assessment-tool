4db06ee1955f1a2cc58465f8698e86df6edd70a3
Richard Lee <richard.lee@aecom.com>
2016-05-12 10:39:32 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# acro
+Acronym Finder
---
+++ imports/ui/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return(
+      <div>Using React with Meteor 1.3!</div>
+    )
+  }
+}
